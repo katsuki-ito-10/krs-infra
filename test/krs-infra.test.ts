f17fd9586dbc1cac076d85a81c05a0dc16911f62
katsuki.ito <fordevelopment10@gmail.com>
2025-09-23 20:28:35 +0900

    Initial commit

---
+++ test/krs-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KrsInfra from '../lib/krs-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/krs-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KrsInfra.KrsInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
